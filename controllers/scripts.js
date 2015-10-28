70565c1e33811f9a80b6f4799a8d84ab870da4b7
thiltunen78 <termikhil@gmail.com>
2015-10-28 12:13:35 +0200

    new stuff

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 console.log("here we go!");
 
 //wait document ready event
@@ -10,7 +13,31 @@ $(document).ready(function(){
     $(".about").text("new text");
     
     $("[data-dummy]").html("<p>hello maailma</p>");
+    
+    var setting = {
+        
+        method:"GET",
+        url:"http://localhost:28017/oma/person/",
+        dataType:"jsonp",
+        jsonp:"jsonp"
+    };
+    
+    $.ajax(setting).done(function(data){ // lähetä pyyntö ja käsittele saatu data
+        console.log(data);
+        
+        for(var i=0;i<data.rows.length;i++){
+            
+            var html =  "<tr>" +
+                        "<td>" + data.rows[i].name + "</td>" +
+                        "<td>" + data.rows[i].address + "</td>" +
+                        "<td>" + data.rows[i].age + "</td>" +
+                        "</tr>";
+            
+            $(html).appendTo("tbody");
+        };
+    }); 
 })
+
 
 /*
 $(document).ready(domReady);
